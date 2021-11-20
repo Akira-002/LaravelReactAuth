acbf8077b92cc9820093ab64e341124512de2a69
Akira-002 <alcumgmn@gmail.com>
2021-11-21 02:55:27 +0900

    ベースの作成

---
+++ resources/js/components/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthApp from './Auth/AuthApp';
+import '../../sass/style.scss';
+
+ReactDOM.render(
+  <AuthApp/>, document.getElementById('portfolioproject')
+);
